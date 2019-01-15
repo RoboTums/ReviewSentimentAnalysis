a99accde06a15af619eb415d8e144d78b26b77ff
Daniel Firebanks-Quevedo <31460539+thefirebanks@users.noreply.github.com>
2019-01-15 15:23:24 +0100

    scrappyscrape

--- scrape_coursera.py
+++ scrape_coursera.py
@@ -1,7 +1,7 @@
 from CourseraClass import Course
 
 def main():
-    course_url = ""
+    course_url = "https://www.coursera.org/learn/machine-learning"
     course = Course(course_url)
     course.get_reviews(10)
     course.build_df()
