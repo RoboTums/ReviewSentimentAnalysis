f83c761ae76793b691c63db4aa85ac97b53491ba
Daniel Firebanks-Quevedo <31460539+thefirebanks@users.noreply.github.com>
2019-01-21 02:23:53 +0100

    Updated name logic

--- scrape_coursera_reviews.py
+++ scrape_coursera_reviews.py
@@ -11,8 +11,8 @@ def main():
     course.get_info()
     course.get_reviews(10)
     course.print_some_reviews(20)
-    course.build_df()
-    course.export_df(file_path="Insert_file_path_here!")
-    course.build_json(file_path="Optional")  # Works all the way till here!
+    course.build_reviews_df()
+    course.export_reviews_df(file_path="Insert_file_path_here!")
+    course.build_course_json(file_path="Optional")  # Works all the way till here!
 
 main()
