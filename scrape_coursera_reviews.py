ac068a96ca13a070312f6b51ed8e44f9405fd0f8
Daniel Firebanks-Quevedo <31460539+thefirebanks@users.noreply.github.com>
2019-01-21 14:58:45 +0100

    Update scrape_coursera_reviews.py

--- scrape_coursera_reviews.py
+++ scrape_coursera_reviews.py
@@ -7,7 +7,7 @@ from CourseraClass import CourseraCourse
 
 def main():
     course_url = "https://www.coursera.org/learn/python-data-analysis"
-    course = CourseraCourse(course_url)
+    course = CourseraCourse(course_url, driver_path="Write_here")
     course.get_info()
     course.get_reviews(10)
     course.print_some_reviews(20)
